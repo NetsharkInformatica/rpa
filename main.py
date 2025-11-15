0035c9bea76d4484dd4e42de1da0c950b7e5f12e
netsharkinformatica <helio.tome2024@gmail.com>
2025-11-15 17:33:26 -0300

    testando bibliotecas

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import pyautogui
+import pyautogui as p
+import time
 import rpa
 import pandas as pd
 
 
+p.sleep(2)
+
+#print(p.position())
+# Point(x=341, y=211)
+p.moveTo(341, 211, duration=1.5)
